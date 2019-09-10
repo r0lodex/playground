24f746fc3a2a14cb500bcf995f0fadbd2ec96b96
Irfan Radzi <irfan@Rolomac.local>
2019-09-10 21:53:11 +0800

    initial init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(KestrelApp());
+
+class KestrelApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
